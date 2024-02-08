b084a428e2b4360aed083c08c7d5a0bff903f6bc
Nathanael Herrera <NathanaeldHerrera@users.noreply.github.com>
2024-02-08 11:18:31 -0700

    Create BioConduct_Contig.aln.R

---
+++ 4. Multi Sequence Alignments/BioConduct_Contig.aln.R
@@ -0,0 +1,19 @@
+library(Biostrings)
+library(seqinr)
+
+fastafiles <- list.files(pattern = ".fa") # this will pull any file with a .fa extension
+fastafiles
+### This will iterate through your fasta files and it will pull out each contig from each species file and append them to a 
+### contig specific file. 
+for (fasta in fastafiles) {
+  sample <- readDNAStringSet(fasta)
+  name <- strsplit(fasta, "_")[[1]][1]
+    for(i in names(sample)){
+#      print(sample[i])
+#      print(name) 
+      a <- sample[i]
+#      print(a) 
+      names(a) <- paste(name)
+      writeXStringSet(a, filepath = paste(i, ".fasta", sep = ''), append = TRUE, format="fasta")
+  }
+}
