6fcc8fd3b1fc3907e225eb304dc37b9afb81748b
Nathanael Herrera <NathanaeldHerrera@users.noreply.github.com>
2024-02-15 14:06:58 -0700

    Update vcf_metrics_plot.r

--- 1. ddRAD Processing and Analyses/vcf_metrics_plot.r
+++ 1. ddRAD Processing and Analyses/vcf_metrics_plot.r
@@ -7,7 +7,8 @@ run_date <- format(Sys.Date(), "%d%b%Y")
 
 # Mean depth per Individual
 ind_depth <- read_delim("./tamias_178Ind_pop.idepth", delim = "\t",
-                        col_names = c("ind", "nsites", "depth"), skip = 1)
+                        col_names = c("ind", "nsites", "depth"), skip = 1, 
+                        show_col_types = FALSE)
 
 # Plot for right here
 a <- ggplot(ind_depth, aes(depth)) + geom_histogram(fill = "dodgerblue1", colour = "black", alpha = 0.3)
@@ -21,7 +22,8 @@ dev.off()
 
 # Proportion of missing data per individual
 ind_miss  <- read_delim("./tamias_178Ind_pop.imiss", delim = "\t",
-                        col_names = c("ind", "ndata", "nfiltered", "nmiss", "fmiss"), skip = 1)
+                        col_names = c("ind", "ndata", "nfiltered", "nmiss", "fmiss"), 
+                        skip = 1, show_col_types = FALSE)
 
 # Plot for right here
 a <- ggplot(ind_miss, aes(fmiss)) + geom_histogram(fill = "dodgerblue1", colour = "black", alpha = 0.3)
@@ -35,7 +37,8 @@ dev.off()
 
 # Variant mean depth
 var_depth <- read_delim("./tamias_178Ind_pop.ldepth.mean", delim = "\t",
-           col_names = c("chr", "pos", "mean_depth", "var_depth"), skip = 1)
+           col_names = c("chr", "pos", "mean_depth", "var_depth"), skip = 1, 
+           show_col_types = FALSE)
 
 # Summarize mean Depth 
 summary(var_depth$mean_depth)
@@ -53,7 +56,8 @@ dev.off()
 
 # Per site variant missingness
 var_miss <- read_delim("./tamias_178Ind_pop.lmiss", delim = "\t",
-                       col_names = c("chr", "pos", "nchr", "nfiltered", "nmiss", "fmiss"), skip = 1)
+                       col_names = c("chr", "pos", "nchr", "nfiltered", "nmiss", "fmiss"), 
+                       skip = 1, show_col_types = FALSE)
 
 # Summarize variant missingness
 summary(var_miss$fmiss)
@@ -70,8 +74,9 @@ dev.off()
 
 # Per site (Phred based) quality
 
-var_qual <- read_delim("./tamias_178Ind_pop.lqual", delim = "\t",
-           col_names = c("chr", "pos", "qual"), skip = 1)
+var_qual <- read_delim("./tamias_178Ind_pop.lqual", delim = "\t", 
+            col_names = c("chr", "pos", "qual"), skip = 1, 
+            show_col_types = FALSE)
 
 # Plot for right here 
 a <- ggplot(var_qual, aes(qual)) + geom_density(fill = "dodgerblue1", colour = "black", alpha = 0.3)
